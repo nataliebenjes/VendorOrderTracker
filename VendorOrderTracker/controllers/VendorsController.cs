4a93131b319586201a0674c0a3fbf50a99127dcd
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-21 12:18:21 -0700

    Add index() new() and create() methods to Vendors controller

---
+++ VendorOrderTracker/controllers/VendorsController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendorOrderTracker.Models;
+
+namespace VendorOrderTracker.Controllers
+{
+  public class CategoriesController : Controller
+  {
+
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> _allthevendors = Vendor.GetAll();
+      return View(_allthevendors);
+    }
+
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+  }
+}
